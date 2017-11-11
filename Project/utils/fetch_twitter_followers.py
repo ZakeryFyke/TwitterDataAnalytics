ca03d82c2b603ec3387fd2009b152ca3b0b07aa5
Zak7062 <zak7062@gmail.com>
2017-11-11 11:17:14 -0600

    sorted shared follower csvs for reasons

--- Project/utils/fetch_twitter_followers.py
+++ Project/utils/fetch_twitter_followers.py
@@ -3,40 +3,41 @@ import time
 import csv
 import os
 
-consumer_key =''
-consumer_secret=''
-access_token=''
-access_secret=''
+def fetchtwitterfollowers():
+    consumer_key = ''
+    consumer_secret = ''
+    access_token = ''
+    access_secret = ''
 
-directoryPath = os.getcwd()
+    directoryPath = os.getcwd()
 
-auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+    auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
 
-api = tweepy.API(auth)
+    api = tweepy.API(auth)
 
-screenNames = ["SenSanders", "CoryBooker", "SenWarren", "marcorubio", "SenJohnMcCain", "RandPaul", "SenTedCruz",
-                 "SenSchumer", "KamalaHarris", "timkaine"]
+    screenNames = ["SenSanders", "CoryBooker", "SenWarren", "marcorubio", "SenJohnMcCain", "RandPaul", "SenTedCruz",
+                   "SenSchumer", "KamalaHarris", "timkaine"]
 
-ids = []
-
-for senator in screenNames:
-    i = 0
     ids = []
-    for page in tweepy.Cursor(api.followers_ids, screen_name=senator).pages():
-        print("Currently on page " + str(i) + " for senator " + senator)
-        if i == 100: # Gather 500,000 followers for each person
-            break
-        stringPage = [str(x) for x in page]
-        ids.extend(stringPage)
-        time.sleep(70)
-        i = i + 1
 
+    for senator in screenNames:
+        i = 0
+        ids = []
+        for page in tweepy.Cursor(api.followers_ids, screen_name=senator).pages():
+            print("Currently on page " + str(i) + " for senator " + senator)
+            if i == 100:  # Gather 500,000 followers for each person
+                break
+            stringPage = [str(x) for x in page]
+            ids.extend(stringPage)
+            time.sleep(70)
+            i = i + 1
 
-    with open(directoryPath + "/" + senator + 'followers.csv', 'wb') as f:
-        writer = csv.writer(f)
-        for val in ids:
-            writer.writerow([val])
+        with open(directoryPath + "/" + senator + 'followers.csv', 'wb') as f:
+            writer = csv.writer(f)
+            for val in ids:
+                writer.writerow([val])
+
 
 def sortfollowerscsv():
     os.chdir('..')
@@ -44,7 +45,7 @@ def sortfollowerscsv():
     directoryPath = os.getcwd() + '\SenatorDataSets'
 
     # Grab the follower csvs from the datasets folder
-    csvPaths = [directoryPath + '/' + p for p in os.listdir(directoryPath) if 'followers' in p]
+    csvPaths = [directoryPath + '/' + p for p in os.listdir(directoryPath) if 'followers' in p.lower()]
 
     for path in csvPaths:
         reader = csv.reader(open(path), delimiter=";")
@@ -53,3 +54,5 @@ def sortfollowerscsv():
             writer = csv.writer(f)
             writer.writerows(sortedlist)
         print("Finished sorting " + path)
+
+sortfollowerscsv()
