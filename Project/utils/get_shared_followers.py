cd10f44620decf06149a427601ca5f5324031bed
Zak7062 <zak7062@gmail.com>
2017-11-27 20:01:55 -0600

    Added stop words text file and word cloud code to project

--- Project/utils/get_shared_followers.py
+++ Project/utils/get_shared_followers.py
@@ -5,29 +5,31 @@ os.chdir('..')
 
 directoryPath = os.getcwd()+ '\SenatorDataSets'
 
-# Grab the follower csvs from the datasets folder
-csvPaths = [directoryPath + '/' + p for  p in os.listdir(directoryPath) if 'followers' in p]
+def get_shared_followers():
+    # Grab the follower csvs from the datasets folder
+    csvPaths = [directoryPath + '/' + p for  p in os.listdir(directoryPath) if 'followers' in p]
 
-for i in range(0, len(csvPaths)):
-    f1 = file(csvPaths[i], 'r')
-    csv1 = csv.reader(f1)
-    L1 = [item for sublist in [x for x in csv1] for item in sublist]
-    for j in range(1, len(csvPaths)):
+    for i in range(0, len(csvPaths)):
+        f1 = file(csvPaths[i], 'r')
+        csv1 = csv.reader(f1)
+        L1 = [item for sublist in [x for x in csv1] for item in sublist]
+        for j in range(1, len(csvPaths)):
 
-        f2 = file(csvPaths[j], 'r')
-        csv2 = csv.reader(f2)
-        L2 = [item for sublist in [x for x in csv2] for item in sublist]
+            f2 = file(csvPaths[j], 'r')
+            csv2 = csv.reader(f2)
+            L2 = [item for sublist in [x for x in csv2] for item in sublist]
 
-        f3Path = directoryPath + '/' + os.path.basename(csvPaths[i]).replace("followers.csv", "") + '_' + os.path.basename(csvPaths[j]).replace("followers.csv", "") + '_' +'SharedFollowers.csv'
+            f3Path = directoryPath + '/' + os.path.basename(csvPaths[i]).replace("followers.csv", "") + '_' + os.path.basename(csvPaths[j]).replace("followers.csv", "") + '_' +'SharedFollowers.csv'
 
-        # hold shared follower ids
-        f3 = file(f3Path, 'w')
-        csv3 = csv.writer(f3)
-        L3 = list(set(L1).intersection(L2))
+            # hold shared follower ids
+            f3 = file(f3Path, 'w')
+            csv3 = csv.writer(f3)
+            L3 = list(set(L1).intersection(L2))
 
-        with open(f3Path, 'wb') as f:
-            writer = csv.writer(f)
-            for val in L3:
-                writer.writerow([val])
+            with open(f3Path, 'wb') as f:
+                writer = csv.writer(f)
+                for val in L3:
+                    writer.writerow([val])
 
-        print 'Finished writing shared followers for ' + os.path.basename(csvPaths[i]) + ' and ' + os.path.basename(csvPaths[j])
+            print 'Finished writing shared followers for ' + os.path.basename(csvPaths[i]) + ' and ' + os.path.basename(csvPaths[j])
+
---
+++ Project/utils/tweets_to_word_cloud.py
@@ -0,0 +1,40 @@
+import csv
+from wordcloud import WordCloud, STOPWORDS
+import os
+import matplotlib.pyplot as plt
+from scipy.misc import imread
+
+os.chdir('..')
+
+directoryPath = os.getcwd()+ '\SenatorDataSets'
+
+def create_word_cloud(party, filename, title):
+
+    twitter_mask = imread(os.getcwd() + '/utils/twitter_mask.png', flatten=True)
+
+    filepath = directoryPath + '/' + party + '/' + filename + '.csv'
+
+    with open(os.getcwd() + '/utils/stopwords.txt') as f:
+        extrastops = f.readlines()
+        extrastops = [x.strip() for x in extrastops]
+
+    list = []
+    with open(filepath, 'rb') as f:
+        reader = csv.reader(f)
+        list = '\t'.join([i[0] for i in reader])
+
+    stopwords = set(STOPWORDS)
+    stopwords = stopwords.union(extrastops)
+
+    # Generate a word cloud image
+    wordcloud = WordCloud(stopwords=stopwords,
+                          mask=twitter_mask,
+                          background_color='white').generate(list)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.title(title)
+
+    plt.show()
+
+create_word_cloud('Republicans', 'AllRepublicansTweets', 'Republican Tweet Word Cloud')
+create_word_cloud('Democrats', 'AllDemocratsTweets', 'Democrat Tweet Word Cloud')
